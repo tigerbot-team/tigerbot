1a97684a486f8362805848a51c73d32fbc33b0a9
Shaun Crampton <shaun@cantab.net>
2018-03-24 19:32:40 +0000

    Program propeller form go, reflash propeller after crash.

--- go-controller/pkg/propeller/propeller.go
+++ go-controller/pkg/propeller/propeller.go
@@ -1,10 +1,13 @@
 package propeller
 
 import (
-	"golang.org/x/exp/io/i2c"
 	"fmt"
-	"time"
+	"golang.org/x/exp/io/i2c"
 	"os"
+	"os/exec"
+	"time"
+	"github.com/kr/pty"
+	"io"
 )
 
 // DEVICE_REG_MODE1 = 0x00
@@ -30,7 +33,7 @@ const (
 	RegMotor4 = 25
 )
 
-type Interface interface{
+type Interface interface {
 	SetMotorSpeeds(frontLeft, frontRight, backLeft, backRight int8) error
 }
 
@@ -48,28 +51,81 @@ func New() (Interface, error) {
 		return nil, err
 	}
 
+	prop := &Propeller{
+		dev: dev,
+	}
+
+	err = prop.Flash()
+	if err != nil {
+		return nil, err
+	}
+
+	return prop, nil
+}
+
+func (p *Propeller) Flash() error {
+	fmt.Println("Flashing the propeller")
+	cmd := exec.Command("propman", "/mb3.binary")
+	// For some reason, propman requires a TTY, or it reports success but the propeller
+	// doesn't actually boot.  Wrap propman with a PTY...
+	f, err := pty.Start(cmd)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("propman output:\n")
+	go io.Copy(os.Stdout, f)
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+	fmt.Println("Flashed the propeller")
+	if err != nil {
+		return err
+	}
+	err =  p.enableResetPin()
+	if err != nil {
+		return err
+	}
+	// Give propeller time to boot...
+	time.Sleep(25*time.Millisecond)
+	return nil
+}
+
+func (p *Propeller) enableResetPin() error {
 	// Ask the kernel to give us control over the GPIO pin that is connected to the Propeller's reset pin.
+	fmt.Println("Taking control of propeller reset pin")
 	export, err := os.OpenFile("/sys/class/gpio/export", os.O_WRONLY, 0666)
 	defer export.Close()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	_, _ = export.WriteString("17") // Propeller hat reset pin; ignore error, will fail if already exported
 
 	// Reset is active LOW, so we want to drive it HIGH to prevent the propeller from resetting.
 	// To do a glitch-free write to the pin, we write "high" to the direction control file.  This
 	// ensures that the pin is driven high as soon as it becomes an output.
+	fmt.Println("Setting propeller reset pin high (i.e. don't reset)")
 	dirn, err := os.OpenFile("/sys/class/gpio/gpio17/direction", os.O_WRONLY, 0666)
 	defer dirn.Close()
 	_, err = dirn.WriteString("high")
 	if err != nil {
 		fmt.Println("Failed to drive propeller reset pin")
-		return nil, err
+		return err
 	}
+	return nil
+}
 
-	return &Propeller{
-		dev: dev,
-	}, nil
+func (p *Propeller) Reset() error {
+	// Reset is active LOW.
+	fmt.Println("Resetting the propeller")
+	value, err := os.OpenFile("/sys/class/gpio/gpio17/value", os.O_WRONLY, 0666)
+	defer value.Close()
+	_, err = value.WriteString("low")
+	if err != nil {
+		fmt.Println("Failed to drive propeller reset pin")
+		return err
+	}
+	return nil
 }
 
 func (p *Propeller) SetMotorSpeeds(frontLeft, frontRight, backLeft, backRight int8) error {
@@ -87,13 +143,33 @@ func (p *Propeller) SetMotorSpeeds(frontLeft, frontRight, backLeft, backRight in
 		frontRight = -127
 	}
 	data := []byte{RegMotor1, byte(-backLeft), byte(-frontLeft), byte(frontRight), byte(backRight)}
-	for {
-		err := p.dev.Write(data)
-		if err == nil {
-			return nil
+	var err error
+	for flashTries := 0; flashTries < 3; flashTries++ {
+		for tries := 0; tries < 20; tries++ {
+			err = p.dev.Write(data)
+			if err == nil {
+				if tries > 0 || flashTries > 0 {
+					fmt.Println("Successfully programmed propeller after retries")
+				}
+				return nil
+			}
+			fmt.Println("Failed to program propeller:", err)
+			time.Sleep(1 * time.Millisecond)
+			p.dev.Close()
+			dev, err := i2c.Open(&i2c.Devfs{"/dev/i2c-1"}, PropAddr)
+			if err != nil {
+				continue
+			}
+			p.dev = dev
 		}
-		time.Sleep(10 * time.Microsecond)
+		// Kill the propeller, in case it's going crazy...
+		p.Reset()
+
+		// Then reflash it...
+		fmt.Println("Failed to program propeller after retries (%v)!!!  Rebooting it!!!", err)
+		p.Flash()
 	}
+	panic("Failed to program or reflash the propeller")
 }
 
 type dummyPropeller struct {
