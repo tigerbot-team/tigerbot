9f06373dd09ff571ed6a6ff4f603d26d73229dc1
Neil Jerram <neil@tigera.io>
2018-04-01 21:28:33 +0000

    Continuing experimentation

--- camera-experiments/center_of_shape.py
+++ camera-experiments/center_of_shape.py
@@ -75,14 +75,17 @@ args = vars(ap.parse_args())
 
 # Load the image and convert it to HSV.
 image = cv2.imread(args["image"])
+image = imutils.resize(image, width=600)
 cv2.imshow("Original", image)
+cv2.waitKey(0)
+
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # Plausible hue ranges.
-yellow = [ 10, 40, "yellow", 60 ]
-green = [ 60, 90, "green", 95 ]
-blue = [ 95, 170, "blue", 140 ]
-orange = [ 220, 260, "orange", 180 ]
+yellow = [ 21, 42, "yellow", 60 ]
+green = [ 42, 63, "green", 95 ]
+blue = [ 66, 119, "blue", 140 ]
+orange = [ 154, 182, "orange", 180 ]
 
 # The H amount by which we move the sliding window for each calculation.
 h_step = 4
@@ -142,26 +145,102 @@ def find_ball(colour):
     return best_x, best_y, highest_value
 
 def find_ball_in_h_range(h_lo, h_hi):
-    if h_hi > h_lo:
-        mask = hue_mask(hsv, h_lo, h_hi)
-    else:
-        mask1 = hue_mask(hsv, h_lo, 256)
-        mask2 = hue_mask(hsv, 0, h_hi)
-        mask = cv2.bitwise_or(mask1, mask2)
+    return find_ball_in_range(h_lo, h_hi, 60, 255, 60, 255)
 
+def find_ball_in_range(h_lo, h_hi, s_lo, s_hi, v_lo, v_hi):
+    mask = hsv_mask(hsv, h_lo, h_hi, s_lo, s_hi, v_lo, v_hi)
     convolution = cv2.filter2D(mask, -1, kernel)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(convolution)
     return maxLoc[0], maxLoc[1], maxVal, mask
 
-def hue_mask(hsv, h_lo, h_hi):
+def hue_mask(hsv, h_lo, h_hi, s_lo, s_hi, v_lo, v_hi):
     return cv2.inRange(hsv,
-                       np.array([int(h_lo*180/255), 60, 60]),
-                       np.array([int(h_hi*180/255), 255, 255]))
+                       np.array([h_lo, s_lo, v_lo]),
+                       np.array([h_hi, s_hi, v_hi]))
 
-cv2.imshow("Image", hsv)
-cv2.waitKey(0)
+def hsv_mask(hsv, h_lo, h_hi, s_lo, s_hi, v_lo, v_hi):
+    if h_hi > h_lo:
+        return hue_mask(hsv, h_lo, h_hi, s_lo, s_hi, v_lo, v_hi)
+    else:
+        mask1 = hue_mask(hsv, h_lo, 180, s_lo, s_hi, v_lo, v_hi)
+        mask2 = hue_mask(hsv, 0, h_hi, s_lo, s_hi, v_lo, v_hi)
+        return cv2.bitwise_or(mask1, mask2)
 
-find_ball(yellow)
-find_ball(green)
-find_ball(blue)
-find_ball(orange)
+def mouse_callback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print x, y, hsv[y,x]
+
+def find_ball2(colour):
+    print "Looking for %r" % colour
+    h_lo = colour[0]
+    h_hi = colour[1]
+    s_lo = 60
+    s_hi = 255
+    v_lo = 60
+    v_hi = 255
+    while True:
+        mask = hsv_mask(hsv, h_lo, h_hi, s_lo, s_hi, v_lo, v_hi)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+        masked = cv2.cvtColor(cv2.bitwise_and(hsv, hsv, mask=mask),
+                              cv2.COLOR_HSV2BGR)
+
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(masked, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(masked, center, 5, (0, 0, 255), -1)
+
+        desc = "%s H=%r..%r S=%r..%r V=%r..%r" % (colour[2],
+                                                  h_lo,
+                                                  h_hi,
+                                                  s_lo,
+                                                  s_hi,
+                                                  v_lo,
+                                                  v_hi)
+        cv2.imshow(desc, masked)
+        cv2.setMouseCallback(desc, mouse_callback)
+        while True:
+            code = cv2.waitKey(0)
+            print "Key pressed: %r" % code
+            if code == 110:
+                break
+        print desc
+        newinput = raw_input("Now try? ")
+        if not newinput:
+            break
+        newranges = newinput.split()
+        print "%r" % newranges
+        h_lo = int(newranges[0])
+        h_hi = int(newranges[1])
+        s_lo = int(newranges[2])
+        s_hi = int(newranges[3])
+        v_lo = int(newranges[4])
+        v_hi = int(newranges[5])
+
+print "Image shape = %r" % [image.shape]
+print "HSV shape = %r" % [hsv.shape]
+
+find_ball2(yellow)
+find_ball2(green)
+find_ball2(blue)
+find_ball2(orange)
