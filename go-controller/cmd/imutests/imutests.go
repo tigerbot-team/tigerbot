43ab751cbad308d5320a15bceec16c9eff0da87a
Shaun Crampton <shaun@cantab.net>
2024-03-16 20:14:07 +0000

    WIP on new IMU.

--- go-controller/cmd/imutests/imutests.go
+++ go-controller/cmd/imutests/imutests.go
@@ -1,87 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/binary"
+	"context"
 	"fmt"
-	"io"
+	"time"
 
-	"go.bug.st/serial"
+	"github.com/tigerbot-team/tigerbot/go-controller/pkg/bno08x"
 )
 
-type IMUReport struct {
-	Index  uint8
-	Yaw    int16
-	Pitch  int16
-	Roll   int16
-	XAccel int16
-	YAccel int16
-	ZAccel int16
-}
-
-func (i *IMUReport) String() string {
-	return fmt.Sprintf("%02x Y:%7.2f P:%7.2f R:%7.2f X:%7.2f Y:%7.2f Z:%7.2f",
-		i.Index, float64(i.Yaw)/100.0, float64(i.Pitch)/100.0, float64(i.Roll)/100.0,
-		float64(i.XAccel)/100.0, float64(i.YAccel)/100.0, float64(i.ZAccel)/100.0)
-}
-
 func main() {
-	mode := &serial.Mode{
-		BaudRate: 115200,
-	}
-	s, err := serial.Open("/dev/ttyAMA0", mode)
-	if err != nil {
-		panic(err)
-	}
-
-	br := bufio.NewReader(s)
-resync:
-	fmt.Printf("Resync...")
+	imu := bno08x.New()
+	go imu.LoopReadingReports(context.Background())
 	for {
-		fmt.Printf(".")
-		buf, err := br.Peek(2)
-		if err != nil {
-			panic(err)
-		}
-		if bytes.Equal(buf, []byte{0xaa, 0xaa}) {
-			break
-		}
-		_, _ = br.Discard(1)
-	}
-	fmt.Printf("\nGot packet start.")
-
-	const packetLen = 19
-	buf := make([]byte, packetLen)
-	var i int
-	for {
-		_, err := io.ReadAtLeast(br, buf, packetLen)
-		if err != nil {
-			panic(err)
-		}
-		if !bytes.Equal(buf[:2], []byte{0xaa, 0xaa}) {
-			fmt.Println("Lost sync.")
-			goto resync
-		}
-		//fmt.Printf("Packet: %x\n", buf)
-		var checksum uint8
-		for _, b := range buf[2 : packetLen-1] {
-			checksum += b
-		}
-		if buf[18] != checksum {
-			fmt.Printf("  BAD CHECKSUM %x != %x\n", buf[18], checksum)
-		}
-		var report IMUReport
-		report.Index = buf[2]
-		report.Yaw = int16(binary.LittleEndian.Uint16(buf[3:5]))
-		report.Pitch = int16(binary.LittleEndian.Uint16(buf[5:7]))
-		report.Roll = int16(binary.LittleEndian.Uint16(buf[7:9]))
-		report.XAccel = int16(binary.LittleEndian.Uint16(buf[9:11]))
-		report.YAccel = int16(binary.LittleEndian.Uint16(buf[11:13]))
-		report.ZAccel = int16(binary.LittleEndian.Uint16(buf[13:15]))
-		if i%10 == 0 {
-			fmt.Printf("Report: %s\n", report.String())
-		}
-		i++
+		r := imu.CurrentReport()
+		fmt.Printf("%v\n", r)
+		time.Sleep(200 * time.Millisecond)
 	}
 }
---
+++ go-controller/pkg/bno08x/bno08x.go
@@ -0,0 +1,143 @@
+package bno08x
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	"go.bug.st/serial"
+)
+
+const serialDevice = "/dev/ttyAMA0"
+
+type IMUReport struct {
+	Time   time.Time
+	Index  uint8
+	Yaw    int16
+	Pitch  int16
+	Roll   int16
+	XAccel int16
+	YAccel int16
+	ZAccel int16
+}
+
+var startTime = time.Now()
+
+func (i IMUReport) String() string {
+	return fmt.Sprintf("%s [%02x] Y:%7.2f P:%7.2f R:%7.2f X:%7.2f Y:%7.2f Z:%7.2f",
+		time.Since(startTime).Round(time.Millisecond), i.Index, float64(i.Yaw)/100.0, float64(i.Pitch)/100.0, float64(i.Roll)/100.0,
+		float64(i.XAccel)/100.0, float64(i.YAccel)/100.0, float64(i.ZAccel)/100.0)
+}
+
+type Interface interface {
+	CurrentReport() IMUReport
+}
+
+type BNO08X struct {
+	lock       sync.Mutex
+	cond       *sync.Cond
+	lastReport IMUReport
+}
+
+var _ Interface = (*BNO08X)(nil)
+
+func New() *BNO08X {
+	b := &BNO08X{}
+	b.cond = sync.NewCond(&b.lock)
+	return b
+}
+
+func (b *BNO08X) CurrentReport() IMUReport {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	return b.lastReport
+}
+
+func (b *BNO08X) LoopReadingReports(ctx context.Context) {
+	for ctx.Err() == nil {
+		err := b.openAndLoop(ctx)
+		if ctx.Err() != nil {
+			return
+		}
+		fmt.Println("BNO08X loop stopped; will retry", err)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func (b *BNO08X) openAndLoop(ctx context.Context) interface{} {
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+	s, err := serial.Open(serialDevice, mode)
+	if err != nil {
+		return fmt.Errorf("failed to open serial port %s: %w", serialDevice, err)
+	}
+
+	br := bufio.NewReader(s)
+resync:
+	fmt.Println("BNO08X Resync...")
+	for {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		buf, err := br.Peek(2)
+		if err != nil {
+			return fmt.Errorf("failed to read from serial: %w", err)
+		}
+		if bytes.Equal(buf, []byte{0xaa, 0xaa}) {
+			break
+		}
+		_, err = br.Discard(1)
+		if err != nil {
+			return fmt.Errorf("failed to read from serial: %w", err)
+		}
+	}
+	fmt.Println("BNO08X: In sync with packet stream.")
+
+	const packetLen = 19
+	buf := make([]byte, packetLen)
+	for {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		_, err := io.ReadAtLeast(br, buf, packetLen)
+		if err != nil {
+			return fmt.Errorf("failed to read from serial: %w", err)
+		}
+		if !bytes.Equal(buf[:2], []byte{0xaa, 0xaa}) {
+			fmt.Println("BNO08X: Lost sync?!")
+			goto resync
+		}
+		//fmt.Printf("Packet: %x\n", buf)
+		var checksum uint8
+		for _, b := range buf[2 : packetLen-1] {
+			checksum += b
+		}
+		if buf[18] != checksum {
+			fmt.Printf("BNO08X:  BAD CHECKSUM %x != %x\n", buf[18], checksum)
+			goto resync
+		}
+		var report IMUReport
+		report.Time = time.Now()
+		report.Index = buf[2]
+		report.Yaw = int16(binary.LittleEndian.Uint16(buf[3:5]))
+		report.Pitch = int16(binary.LittleEndian.Uint16(buf[5:7]))
+		report.Roll = int16(binary.LittleEndian.Uint16(buf[7:9]))
+		report.XAccel = int16(binary.LittleEndian.Uint16(buf[9:11]))
+		report.YAccel = int16(binary.LittleEndian.Uint16(buf[11:13]))
+		report.ZAccel = int16(binary.LittleEndian.Uint16(buf[13:15]))
+		b.setReport(report)
+	}
+}
+
+func (b *BNO08X) setReport(report IMUReport) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	b.lastReport = report
+	b.cond.Broadcast()
+}
