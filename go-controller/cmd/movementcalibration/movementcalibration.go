3d4744e92ec9e719ec91d9a3109810f16cd320ec
Nell Jerram <nelljerram@cantab.net>
2024-04-13 19:04:55 +0100

    Fixes

--- go-controller/cmd/movementcalibration/movementcalibration.go
+++ go-controller/cmd/movementcalibration/movementcalibration.go
@@ -77,17 +77,17 @@ func main() {
 	hh := hw.StartHeadingHoldMode()
 	throttleSpeed := 100 // Just need a slow walking speed here.
 
-	for i := 0; i < 12; i++ {
+	for i := 1; i < 12; i++ {
 		angle := [2]int{i*15 - 180, i * 15}
 		index := [2]int{i, i + 12}
 		fmt.Printf("Measurements %v/36: angles %v, %v...\n", i+1, angle[0], angle[1])
-		for dir := 0; dir < 2; dir++ {
+		for dir := 1; dir >= 0; dir-- {
 			for j := 0; j < 1; j++ {
 				fmt.Printf("%v/2:\n", 1+dir)
 				startRotations := hw.AccumulatedRotations()
 				hh.SetThrottleWithAngle(float64(throttleSpeed), float64(angle[dir]))
 				time.Sleep(time.Duration(4) * time.Second)
-				hw.StopMotorControl()
+				hh.SetThrottle(0)
 				endRotations := hw.AccumulatedRotations()
 				table[index[dir]][j].aheadMM, table[index[dir]][j].leftMM = getDisplacements()
 				for k := range endRotations {
@@ -105,6 +105,6 @@ func main() {
 	}
 }
 
-func printRow(index int, row [2]measurements) {
+func printRow(index int, row [1]measurements) {
 	fmt.Printf("table[%v] = %#v\n", index, row)
 }
